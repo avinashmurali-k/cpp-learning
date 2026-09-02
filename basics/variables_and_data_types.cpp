6af5db29d7716ae76d40e5eb3e2705b862d58315
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-02 20:59:29 +0530

    Add variables_and_data_types.cpp

---
+++ basics/variables_and_data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string name = "Avinash";
+    int age = 20;
+    double height = 6.1;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
