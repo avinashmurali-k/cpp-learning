c691ec2250f79f64509d9bd24403b61825f7b0f1
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-02 21:08:30 +0530

    Add substring.cpp

---
+++ strings/substring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string phrase = "Giraffe Academy";
+
+    string phraseSub = phrase.substr(8, 3);
+
+    cout << phraseSub;
+
+    return 0;
+}
