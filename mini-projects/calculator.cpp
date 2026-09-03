b5160267a32482126aee8f0e05d90260e8adba03
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-03 21:27:43 +0530

    Improve calculator with operator selection

--- mini-projects/calculator.cpp
+++ mini-projects/calculator.cpp
@@ -2,16 +2,33 @@
 using namespace std;
 
 int main() {
-
-    double num1, num2;
+    int num1, num2;
+    char op;
 
     cout << "Enter the first number: ";
     cin >> num1;
 
+    cout << "Enter the operator: ";
+    cin >> op;
+
     cout << "Enter the second number: ";
     cin >> num2;
 
-    cout << num1 + num2;
+    int result;
+
+    if (op == '+') {
+        result = num1 + num2;
+    } else if (op == '-') {
+        result = num1 - num2;
+    } else if (op == '/') {
+        result = num1 / num2;
+    } else if (op == '*') {
+        result = num1 * num2;
+    } else {
+        cout << "Invalid operator";
+    }
+
+    cout << result;
 
     return 0;
 }
