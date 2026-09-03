4fb0ff7875e2b2607dffdb6a70da689372af8ddb
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-03 13:19:54 +0530

    Add functions.cpp

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -1,20 +1,14 @@
 #include <iostream>
 using namespace std;
 
-// Function declaration / prototype
-void sayHi(string name, int age);
-
-int main() {
-
-    // Calling a function
-    sayHi("Mike", 20);
-    sayHi("Tom", 60);
-    sayHi("Steve", 35);
-
-    return 0;
+double cube(double num) {
+    double result = num * num * num;
+    return result;
 }
 
-// Function definition
-void sayHi(string name, int age) {
-    cout << "Hello " << name << " You are " << age << endl;
+int main() {
+    double answer = cube(5.0);
+    cout << answer << endl;
+
+    return 0;
 }
