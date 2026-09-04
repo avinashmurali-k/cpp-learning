5cf7d0539ec8020ec1e0a864e80b4941144ad4d2
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-04 20:50:28 +0530

    Add for loop examples

---
+++ loops/for_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Basic for loop
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
+    }
+
+    // Using a for loop with an array
+    int nums[] = {1, 3, 5, 6, 8};
+
+    for (int i = 0; i < 5; i++) {
+        cout << nums[i] << endl;
+    }
+
+    return 0;
+}
