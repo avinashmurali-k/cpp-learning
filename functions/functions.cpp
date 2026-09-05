871c00bb908a91379b3c6806353ddb2953694f55
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-05 22:43:43 +0530

    Add exponent function using for loop

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 using namespace std;
 
+void sayHi(string name, int age) {
+    cout << "Hello " << name << " You are " << age << endl;
+}
+
 double cube(double num) {
     double result = num * num * num;
     return result;
 }
 
+int power(int baseNum, int powNum) {
+    int result = 1;
+
+    for (int i = 0; i < powNum; i++) {
+        result = result * baseNum;
+    }
+
+    return result;
+}
+
 int main() {
-    double answer = cube(5.0);
-    cout << answer << endl;
+    sayHi("Mike", 20);
+    sayHi("Tom", 60);
+    sayHi("Steve", 35);
+
+    cout << cube(5.0) << endl;
+    cout << power(10, 2) << endl;
 
     return 0;
 }
