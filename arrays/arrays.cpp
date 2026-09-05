349d0c29ed8caf7ac24f79244825c292b566d61b
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-05 22:44:27 +0530

    Add 2D arrays and nested loops

--- arrays/arrays.cpp
+++ arrays/arrays.cpp
@@ -3,27 +3,32 @@ using namespace std;
 
 int main() {
 
-    // Declaring and initializing an array
+    // 1D array
     int luckyNums[] = {1, 3, 7, 35, 6, 8};
 
-    // Accessing an array element
     cout << "Element at index 3: " << luckyNums[3] << endl;
 
-    // Changing an array element
     luckyNums[3] = 19;
+
     cout << "After changing index 3: " << luckyNums[3] << endl;
 
-    // Creating an array with a specific size
-    int numbers[20] = {1, 3, 7, 35, 6, 8};
+    // 2D array
+    int numberGrid[3][2] = {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
 
-    numbers[10] = 100;
-    cout << "Element at index 10: " << numbers[10] << endl;
+    cout << "Element at [1][1]: " << numberGrid[1][1] << endl;
 
-    // Assigning a value to an array element
-    int values[20];
+    // Nested loops
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << numberGrid[i][j] << " ";
+        }
 
-    values[0] = 100;
-    cout << "First element: " << values[0] << endl;
+        cout << endl;
+    }
 
     return 0;
 }
