bd7e26d6ec690f11abf9c2ff088a1b92a4f61a28
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-05 23:23:07 +0530

    Add constructors and constructor overloading example

--- classes/classes_and_objects.cpp
+++ classes/classes_and_objects.cpp
@@ -3,31 +3,46 @@ using namespace std;
 
 class Car {
 public:
-    string Brand;
+    string brand;
     string color;
     string model;
+
+    // Default constructor
+    Car() {
+        brand = "No Brand";
+        color = "No Color";
+        model = "No Model";
+    }
+
+    // Parameterized constructor
+    Car(string aBrand, string aColor, string aModel) {
+        brand = aBrand;
+        color = aColor;
+        model = aModel;
+    }
 };
 
 int main() {
-    Car car1;
+    // Object using parameterized constructor
+    Car car1("BMW", "black", "M5");
 
-    car1.Brand = "BMW";
-    car1.color = "black";
-    car1.model = "M5";
-
-    Car car2;
-
-    car2.Brand = "AUDI";
-    car2.color = "White";
-    car2.model = "R8";
-
-    cout << car1.Brand << endl;
+    cout << car1.brand << endl;
     cout << car1.color << endl;
     cout << car1.model << endl;
 
-    cout << car2.Brand << endl;
+    // Another object using parameterized constructor
+    Car car2("AUDI", "White", "R8");
+
+    cout << car2.brand << endl;
     cout << car2.color << endl;
     cout << car2.model << endl;
+
+    // Object using default constructor
+    Car car3;
+
+    cout << car3.brand << endl;
+    cout << car3.color << endl;
+    cout << car3.model << endl;
 
     return 0;
 }
