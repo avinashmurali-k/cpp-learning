5429938eaca94b434d4af71dac838847ffe6a0a5
Avinash Murali K <avinashmurali958@gmail.com>
2026-09-06 08:41:11 +0530

    Add object function example

--- classes/classes_and_objects.cpp
+++ classes/classes_and_objects.cpp
@@ -1,48 +1,33 @@
 #include <iostream>
 using namespace std;
 
-class Car {
+class Student {
 public:
-    string brand;
-    string color;
-    string model;
+    string name;
+    string major;
+    double gpa;
 
-    // Default constructor
-    Car() {
-        brand = "No Brand";
-        color = "No Color";
-        model = "No Model";
+    Student(string aName, string aMajor, double aGpa) {
+        name = aName;
+        major = aMajor;
+        gpa = aGpa;
     }
 
-    // Parameterized constructor
-    Car(string aBrand, string aColor, string aModel) {
-        brand = aBrand;
-        color = aColor;
-        model = aModel;
+    bool hasHonors() {
+        if (gpa >= 3.5) {
+            return true;
+        }
+
+        return false;
     }
 };
 
 int main() {
-    // Object using parameterized constructor
-    Car car1("BMW", "black", "M5");
+    Student student1("Jim", "Business", 2.4);
+    Student student2("Pam", "Art", 3.6);
 
-    cout << car1.brand << endl;
-    cout << car1.color << endl;
-    cout << car1.model << endl;
-
-    // Another object using parameterized constructor
-    Car car2("AUDI", "White", "R8");
-
-    cout << car2.brand << endl;
-    cout << car2.color << endl;
-    cout << car2.model << endl;
-
-    // Object using default constructor
-    Car car3;
-
-    cout << car3.brand << endl;
-    cout << car3.color << endl;
-    cout << car3.model << endl;
+    cout << student1.hasHonors() << endl;
+    cout << student2.hasHonors() << endl;
 
     return 0;
 }
